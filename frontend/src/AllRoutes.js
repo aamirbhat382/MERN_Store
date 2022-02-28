044820f60fbe23efeac19e32ccf2d2dbe0906573
Aamir Bhat <aamirbhat382@gmail.com>
2022-02-28 06:03:41 +0000

    fix cart, add + , - on cart

--- frontend/src/AllRoutes.js
+++ frontend/src/AllRoutes.js
@@ -1,63 +1,68 @@
-import React,  { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./core/Home";
-import Signup from "./user/Signup"
-import Signin from "./user/Signin"
-import {isAutheticated} from './auth/helper/index'
-import AdminRoutes from "./auth/helper/AdminRoutes"
-import PrivateRoutes from "./auth/helper/PrivateRoutes"
-import UserDashBoard from './user/UserDashBoard'
-import AdminDashBoard from './user/AdminDashBoard'
-import AddCategory from'./admin/AddCategory'
-import AddProduct from'./admin/AddProduct'
-import ManageProducts from './admin/ManageProducts'
-import UpdateProduct from './admin/UpdateProduct'
-import { CartContext } from './CartContext';
-import { getCart, storeCart } from './helper';
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import { isAutheticated } from "./auth/helper/index";
+import AdminRoutes from "./auth/helper/AdminRoutes";
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
+import AddProduct from "./admin/AddProduct";
+import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
+import { CartContext } from "./CartContext";
+import { getCart, storeCart } from "./helper";
 import Cart from "./user/Cart";
 
 const AllRoutes = () => {
-  const [ cart, setCart ] = useState({});
+  const [cart, setCart] = useState({});
   // Fetch cart from local storage
   useEffect(() => {
-    getCart().then(cart => {
+    getCart().then((cart) => {
       setCart(JSON.parse(cart));
     });
   }, []);
-  
+
   useEffect(() => {
-      storeCart(JSON.stringify(cart));
+    storeCart(cart);
   }, [cart]);
 
   return (
-     <BrowserRouter>
-     <CartContext.Provider value={{ cart, setCart }}>
-      <Routes>
-        <Route path="/"  element={<Home />}>
-        </Route>
-        <Route path="/cart"  element={<Cart/>}>
-        </Route>
-       
-        <Route path='/signup' element={<Signup />}>
-        </Route>
-        <Route path="/signin"  element={<Signin />}>
-        </Route>
-         <Route path="/admin/dashboard" element={<AdminRoutes> <AdminDashBoard/> </AdminRoutes>}>
-            <Route path="home"  element={< AddCategory/>}/>
-            <Route path="create/category"  element={< AddCategory/>}/>
-            <Route path="create/product"  element={< AddProduct/>}/>
-            <Route path="products"  element={< ManageProducts/>}>
-              <Route path="update/:productId"  element={<UpdateProduct/>}/>
+    <BrowserRouter>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route
+            path="/admin/dashboard"
+            element={
+              <AdminRoutes>
+                {" "}
+                <AdminDashBoard />{" "}
+              </AdminRoutes>
+            }
+          >
+            <Route path="home" element={<AddCategory />} />
+            <Route path="create/category" element={<AddCategory />} />
+            <Route path="create/product" element={<AddProduct />} />
+            <Route path="products" element={<ManageProducts />}>
+              <Route path="update/:productId" element={<UpdateProduct />} />
             </Route>
-            
-        </Route>
-          <Route path="/user/dashboard" element={
-              <PrivateRoutes >
-                <UserDashBoard/>
+          </Route>
+          <Route
+            path="/user/dashboard"
+            element={
+              <PrivateRoutes>
+                <UserDashBoard />
               </PrivateRoutes>
             }
           ></Route>
-      </Routes>
+        </Routes>
       </CartContext.Provider>
     </BrowserRouter>
   );
--- frontend/src/user/Cart.js
+++ frontend/src/user/Cart.js
@@ -30,8 +30,53 @@ function Cart() {
         togglePriceFetched(true);
       });
   }, [cart, priceFetched]);
-  console.log(products);
 
+  const getQty = (productId) => {
+    return cart.items[productId];
+}
+
+const increment = (productId) => {
+    const existingQty = cart.items[productId];
+    const _cart = {...cart};
+    _cart.items[productId] = existingQty + 1;
+    _cart.totalItems += 1;
+    setCart(_cart);
+}
+
+const decrement = (productId) => {
+    const existingQty = cart.items[productId];
+   if (existingQty === 1) {
+        return;
+   }
+    const _cart = {...cart};
+    _cart.items[productId] = existingQty - 1;
+    _cart.totalItems -= 1;
+    setCart(_cart);
+}
+
+const getSum = (productId, price) => {
+    const sum = price * getQty(productId);
+    total += sum;
+    return sum;
+}
+
+const handleDelete = (productId) => {
+    const _cart = {...cart};
+    const qty = _cart.items[productId];
+    delete _cart.items[productId];
+    _cart.totalItems -= qty;
+    setCart(_cart);
+    const updatedProductsList = products.filter((product) => product._id !== productId);
+    setProducts(updatedProductsList);
+}
+
+const handleOrderNow = () => {
+    window.alert('Order placed succesfully!');
+    setProducts([]);
+    setCart({});
+}
+  
+  
   return (
     <Base>
       <div className="container py-3">
@@ -39,12 +84,22 @@ function Cart() {
           {products &&
             products.map((product) => {
               return (
-                <li key={product._id} className="list-group-item d-flex justify-content-between align-items-start">
+                <li
+                  key={product._id}
+                  className="list-group-item d-flex justify-content-between align-items-start"
+                >
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">{product.name}</div>
                     {product.price}
                   </div>
-                  <span className="badge bg-primary rounded-pill">14</span>
+                  <div className="qty-price me-auto">
+                  <div className="fw-bold">{ getQty(product._id) }</div>
+                  ₹ { getSum(product._id, product.price) }
+                  </div>
+                  <div className="btn-group">
+                  <button onClick={() => { decrement(product._id) }} className="btn btn-dark btn-sm">-</button>
+                  <button  onClick={() => { increment(product._id) }}  className="btn btn-dark btn-sm">+</button>
+                  </div>
                 </li>
               );
             })}
